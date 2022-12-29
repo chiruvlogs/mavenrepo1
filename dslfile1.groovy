d95eb5bf2edc35f397799b7a58ad6775a875df45
chiruvlogs <115852614+chiruvlogs@users.noreply.github.com>
2022-12-29 18:47:13 -0500

    Create dslfile1.groovy

---
+++ dslfile1.groovy
@@ -0,0 +1,18 @@
+job('SampledslTest') {
+    scm {
+        git('https://github.com/chiruvlogs/mavenrepo1.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('chiruvlogs')
+            node / gitConfigEmail('saichiranjeevi.originhubs@gmail.com')
+        }
+    }
+    //triggers {
+     //   scm('H/5 * * * *')
+   // }
+   // wrappers {
+     //   nodejs('nodejs') // this is the name of the NodeJS installation in 
+       //                  // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    //}
+    steps {
+        shell("java -version")
+    }
+}
