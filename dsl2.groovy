eda529f3667185673d19065435bb30e4735ae8c8
chiruvlogs <115852614+chiruvlogs@users.noreply.github.com>
2022-12-30 10:59:47 -0500

    Update dsl2.groovy

--- dsl2.groovy
+++ dsl2.groovy
@@ -1,14 +1,15 @@
-job('SampledslTest') {
-	  description("This is the task realted to automating creating job using dsl and then executing tasks related to shared repo");
+//job('SampledslTest') {
+	  
 
-    scm {
-        git('https://github.com/sathishvision2029/trainingrepo1.git') 
-        {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('chiruvlogs')
-            node / gitConfigEmail('saichiranjeevi.originhubs@gmail.com')
-        }
-    }
- //pipelineJob{
+    //scm {
+       // git('https://github.com/sathishvision2029/trainingrepo1.git') 
+     //   {  node -> // is hudson.plugins.git.GitSCM
+       //     node / gitConfigName('chiruvlogs')
+     //       node / gitConfigEmail('saichiranjeevi.originhubs@gmail.com')
+    //}
+  //  }
+pipelineJob{
+description("This is the task realted to automating creating job using dsl and then executing tasks related to shared repo");
     definition{
         cpsScm {
             scm {
@@ -24,4 +25,4 @@ job('SampledslTest') {
             scriptPath('chirujenkinfile1')
          }
     //  }
-   }
+  // }
