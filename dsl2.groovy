93715b48a0bd4ba526a61b2abc7cabd419767d98
chiruvlogs <115852614+chiruvlogs@users.noreply.github.com>
2022-12-30 10:51:26 -0500

    Create dsl2.groovy

---
+++ dsl2.groovy
@@ -0,0 +1,27 @@
+job('SampledslTest') {
+	  description("This is the task realted to automating creating job using dsl and then executing tasks related to shared repo");
+
+    scm {
+        git('https://github.com/sathishvision2029/trainingrepo1.git') 
+        {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('chiruvlogs')
+            node / gitConfigEmail('saichiranjeevi.originhubs@gmail.com')
+        }
+    }
+ //pipelineJob{
+    definition{
+        cpsScm {
+            scm {
+              git{
+                remote { url('https://github.com/sathishvision2029/trainingrepo1.git')
+				credentials('cc324e07-392d-4970-bd02-142319fae0e6')
+                  }
+          //branches('master', '**/feature*')
+          //scriptPath('chirujenkinfile1')
+                    }
+                }
+            }
+            scriptPath('chirujenkinfile1')
+         }
+    //  }
+   }
